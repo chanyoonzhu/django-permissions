1c86b51732ac63642a92ae5a8403d87d5a3a8eb2
Chanyoon Zhu <zhqianyu@amazon.com>
2020-07-10 11:37:16 -0700

    restricts update/put/delete permissions

--- permissions_project/settings.py
+++ permissions_project/settings.py
@@ -125,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'ALLOW_ANY', # default
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- snacks/views.py
+++ snacks/views.py
@@ -3,12 +3,16 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 
 from .models import Snack
 from .serializers import SnackSerializer
+from .permissions import IsPurchaserOrReadOnly
 
 # Create your views here.
 class SnackList(ListCreateAPIView):
     queryset = Snack.objects.all() # when asked for snacks, return all snacks (without any filter)
     serializer_class = SnackSerializer
+    permission_classes = (IsPurchaserOrReadOnly, ) # a tuple with a single item needs a comma
 
 class SnackDetail(RetrieveUpdateDestroyAPIView):
     queryset = Snack.objects.all()
     serializer_class = SnackSerializer
+    permission_classes = (IsPurchaserOrReadOnly, ) # a tuple with a single item needs a comma
+
