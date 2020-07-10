2a43e60f715cafa1b400d7324819d0c3dd2d8a83
Chanyoon Zhu <zhqianyu@amazon.com>
2020-07-10 10:40:34 -0700

    adds purchaser as foreign key

--- snacks/serializers.py
+++ snacks/serializers.py
@@ -5,4 +5,4 @@ class SnackSerializer(ModelSerializer):
     
     class Meta:
         model = Snack
-        fields = ('id', 'name') # attributes of the model that we'd like to be exposed
+        fields = ('id', 'name', 'purchaser') # attributes of the model that we'd like to be exposed
