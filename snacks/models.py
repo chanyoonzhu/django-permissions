6ee305cc999d12b0f2535bdabbc00bb8cfc4bd0b
Chanyoon Zhu <zhqianyu@amazon.com>
2020-07-10 09:35:15 -0700

    adds and registers model

--- snacks/models.py
+++ snacks/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Snack(models.Model):
-    pass
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
